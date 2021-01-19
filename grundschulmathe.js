e5997d4d6da808753fb55ea47ad45797354dae1e
Stefan Golker <stefangolker@Stefans-MBP.fritz.box>
2021-01-19 15:52:14 +0100

    Added mulitplication  & Division with the check if there is no rest

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,7 +1,7 @@
 //Aufgabe A
 alert("Grundschulmathe");
 
-//Aufgabe B, C, D
+//Aufgabe B, C, D, E
 
 //Zufallszahl generieren
 function generateRandomNum() {
@@ -69,7 +69,8 @@ function subtraktion(points = 0, numberOfRuns = 0) {
             subtraktion(points, numberOfRuns);
         }
         else if (numberOfRuns === 5 && points >= 4) {
-            return 0;
+            alert("Herzlichen Glückwunsch, du hast das Nächste Level erreicht!")
+            multiplikation();
         }
         else if (numberOfRuns === 5 && points < 4) {
             alert("Du hast leider nicht genug Punkte erreicht.")
@@ -82,13 +83,101 @@ function subtraktion(points = 0, numberOfRuns = 0) {
             subtraktion(points, numberOfRuns);
         }
         else if (numberOfRuns === 5 && points >= 4) {
-            return 0;
+            alert("Herzlichen Glückwunsch, du hast das Nächste Level erreicht!")
+            multiplikation();
         }
         else if (numberOfRuns === 5 && points < 4) {
             alert("Du hast leider nicht genug Punkte erreicht.")
             addition();
         }
 
+    }
+}
+
+//Multiplikation
+function multiplikation(points = 0, numberOfRuns = 0) {
+    var num1 = generateRandomNum();
+    var num2 = generateRandomNum();
+    var solution = num1 * num2;
+    numberOfRuns++;
+
+    var userNum = parseInt(prompt("Wieviel ist " + num1 + " * " + num2 + " ?"), 10);
+
+    if (userNum === solution) {
+        points += 1
+        alert("Richtige Antwort! " + points + "/5");
+        if (numberOfRuns < 5) {
+            multiplikation(points, numberOfRuns);
+        }
+        else if (numberOfRuns === 5 && points >= 4) {
+            alert("Herzlichen Glückwunsch, du hast das Nächste Level erreicht!")
+            Division();
+        }
+        else if (numberOfRuns === 5 && points < 4) {
+            alert("Du hast leider nicht genug Punkte erreicht.")
+            addition();
+        }
+    }
+    else if (userNum !== solution) {
+        alert("Leider Falsch! " + points + "/5");
+        if (numberOfRuns < 5) {
+            multiplikation(points, numberOfRuns);
+        }
+        else if (numberOfRuns === 5 && points >= 4) {
+            alert("Herzlichen Glückwunsch, du hast das Nächste Level erreicht!")
+            Division();
+        }
+        else if (numberOfRuns === 5 && points < 4) {
+            alert("Du hast leider nicht genug Punkte erreicht.")
+            addition();
+        }
+
+    }
+}
+
+//Division
+function Division(points = 0, numberOfRuns = 0) {
+    var num1 = generateRandomNum();
+    var num2 = generateRandomNum();
+    var solution = num1 / num2;
+    
+    //Check if there's no rest
+    if (num1 % num2 == 0) {
+        numberOfRuns++;
+        var userNum = parseInt(prompt("Wieviel ist " + num1 + " / " + num2 + " ?"), 10);
+        if (userNum === solution) {
+            points += 1
+            alert("Richtige Antwort! " + points + "/5");
+            if (numberOfRuns < 5) {
+                Division(points, numberOfRuns);
+            }
+            else if (numberOfRuns === 5 && points >= 4) {
+                alert("Herzlichen Glückwunsch, du hast alle Level abgeschlossen!")
+                return 0;
+            }
+            else if (numberOfRuns === 5 && points < 4) {
+                alert("Du hast leider nicht genug Punkte erreicht.")
+                addition();
+            }
+        }
+        else if (userNum !== solution) {
+            alert("Leider Falsch! " + points + "/5");
+            if (numberOfRuns < 5) {
+                Division(points, numberOfRuns);
+            }
+            else if (numberOfRuns === 5 && points >= 4) {
+                alert("Herzlichen Glückwunsch, du hast alle Level Abgeschlossen!")
+                return 0;
+            }
+            else if (numberOfRuns === 5 && points < 4) {
+                alert("Du hast leider nicht genug Punkte erreicht.")
+                addition();
+            }
+    
+        }
+    }
+    else {
+        Division(points, numberOfRuns);
     }
 }
 
