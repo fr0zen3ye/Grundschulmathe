a3e6748985b9904ff4ac639110f8b04eddef20d0
Stefan Golker <stefangolker@Stefans-MBP.fritz.box>
2021-01-19 15:01:47 +0100

    Task C - Run 5 Times + Points counter

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -5,18 +5,37 @@ function generateRandomNum() {
     return Math.floor(Math.random() * 21);
 }
 
+var numbersOfRuns = 0;
+
+//Addition
 function addition(points = 0, numberOfRuns = 0) {
     var num1 = generateRandomNum();
     var num2 = generateRandomNum();
+    numbersOfRuns++;
     var solution = num1 + num2;
     var userNum = parseInt(prompt("Wieviel ist " + num1 + " + " + num2 + " ?"), 10);
-    
-    if (userNum === solution) {
-        alert("Richtig!");
+
+    if (userNum === solution && numberOfRuns <= 5) {
+        points += 1
+        alert("Richtige Antwort! " + points + "/5");
+
+        if (numbersOfRuns < 5) {
+            addition(points, numbersOfRuns);
+        }
+        else if (numbersOfRuns === 5) {
+            return 0;
+        }
     }
-    else {
-        alert("Falsch!");
+    else if (userNum !== solution && numbersOfRuns <= 5) {
+        alert("Leider Falsch! " + points + "/5");
+        if (numbersOfRuns < 5) {
+            addition(points, numbersOfRuns);
+        }
+        else if (numbersOfRuns === 5) {
+            return 0;
+        }
     }
+
 }
 
 addition();
