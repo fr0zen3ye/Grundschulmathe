b04cd052fd3cb3effa962c0a68f9ecedc0af4369
Stefan Golker <stefangolker@Stefans-MBP.fritz.box>
2021-01-19 14:11:56 +0100

    Funktion Generierung Zufallszahlen & Abfrage von Zufallszahl + Ergebnis

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,2 +1,22 @@
-//Aufgabe A
-alert("Grundschulmathe");
+//Aufgabe B
+
+//Zufallszahl generieren
+function generateRandomNum() {
+    return Math.floor(Math.random() * 21);
+}
+
+function addition(points = 0, numberOfRuns = 0) {
+    var num1 = generateRandomNum();
+    var num2 = generateRandomNum();
+    var solution = num1 + num2;
+    var userNum = parseInt(prompt("Wieviel ist " + num1 + " + " + num2 + " ?"), 10);
+    
+    if (userNum === solution) {
+        alert("Richtig!");
+    }
+    else {
+        alert("Falsch!");
+    }
+}
+
+addition();
