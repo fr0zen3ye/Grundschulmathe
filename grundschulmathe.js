ed50406e6ab586326111cd5cce5bc922ae2fbfef
Stefan Golker <stefangolker@Stefans-MBP.fritz.box>
2021-01-19 15:34:51 +0100

    Fehlerausbesserung und einfügen der Teilaufgabe A

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,39 +1,42 @@
-//Aufgabe B
+//Aufgabe A
+alert("Grundschulmathe");
+
+//Aufgabe B, C, D
 
 //Zufallszahl generieren
 function generateRandomNum() {
     return Math.floor(Math.random() * 21);
 }
-
-var numbersOfRuns = 0;
+points = 0;
 
 //Addition
 function addition(points = 0, numberOfRuns = 0) {
     var num1 = generateRandomNum();
     var num2 = generateRandomNum();
-    numbersOfRuns++;
     var solution = num1 + num2;
+    numberOfRuns++;
     var userNum = parseInt(prompt("Wieviel ist " + num1 + " + " + num2 + " ?"), 10);
 
-    if (userNum === solution && numberOfRuns <= 5) {
+    if (userNum === solution) {
         points += 1
         alert("Richtige Antwort! " + points + "/5");
-
-        if (numbersOfRuns < 5) {
-            addition(points, numbersOfRuns);
+        if (numberOfRuns < 5) {
+            addition(points, numberOfRuns);
         }
-        else if (numbersOfRuns === 5) {
+        else if (numberOfRuns === 5) {
             return 0;
         }
     }
-    else if (userNum !== solution && numbersOfRuns <= 5) {
+    else if (userNum !== solution) {
         alert("Leider Falsch! " + points + "/5");
-        if (numbersOfRuns < 5) {
-            addition(points, numbersOfRuns);
+        if (numberOfRuns < 5) {
+            addition(points, numberOfRuns);
         }
-        else if (numbersOfRuns === 5) {
+        else if (numberOfRuns === 5) {
+            alert("Herzlichen Glückwunsch, du hast das Nächste Level erreicht!")
             return 0;
         }
+
     }
 
 }
