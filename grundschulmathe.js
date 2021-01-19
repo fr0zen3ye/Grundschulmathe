6d813087b0442fe57c115848f57bfc3abbd8371e
Stefan Golker <stefangolker@Stefans-MBP.fritz.box>
2021-01-19 15:30:18 +0100

    fullfills Task D - Subtraction is added

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,41 +1,96 @@
-//Aufgabe B
+//Aufgabe A
+alert("Grundschulmathe");
+
+//Aufgabe B, C, D
 
 //Zufallszahl generieren
 function generateRandomNum() {
     return Math.floor(Math.random() * 21);
 }
-
-var numbersOfRuns = 0;
+points = 0;
 
 //Addition
 function addition(points = 0, numberOfRuns = 0) {
     var num1 = generateRandomNum();
     var num2 = generateRandomNum();
-    numbersOfRuns++;
     var solution = num1 + num2;
+    numberOfRuns++;
     var userNum = parseInt(prompt("Wieviel ist " + num1 + " + " + num2 + " ?"), 10);
 
-    if (userNum === solution && numberOfRuns <= 5) {
+    if (userNum === solution) {
         points += 1
         alert("Richtige Antwort! " + points + "/5");
-
-        if (numbersOfRuns < 5) {
-            addition(points, numbersOfRuns);
+        if (numberOfRuns < 5) {
+            addition(points, numberOfRuns);
         }
-        else if (numbersOfRuns === 5) {
-            return 0;
+        else if (numberOfRuns === 5 && points >= 4) {
+            alert("Herzlichen Glückwunsch, du hast das Nächste Level erreicht!")
+            subtraktion();
+        }
+        else if (numberOfRuns === 5 && points < 4) {
+            alert("Du hast leider nicht genug Punkte erreicht.")
+            addition();
         }
     }
-    else if (userNum !== solution && numbersOfRuns <= 5) {
+    else if (userNum !== solution) {
         alert("Leider Falsch! " + points + "/5");
-        if (numbersOfRuns < 5) {
-            addition(points, numbersOfRuns);
+        if (numberOfRuns < 5) {
+            addition(points, numberOfRuns);
         }
-        else if (numbersOfRuns === 5) {
-            return 0;
+        else if (numberOfRuns === 5 && points >= 4) {
+            alert("Herzlichen Glückwunsch, du hast das Nächste Level erreicht!")
+            subtraktion();
         }
+        else if (numberOfRuns === 5 && points < 4) {
+            alert("Du hast leider nicht genug Punkte erreicht.")
+            addition();
+        }
+
     }
 
 }
 
+//Subtraktion
+
+function subtraktion(points = 0, numberOfRuns = 0) {
+    var num1 = generateRandomNum();
+    var num2 = generateRandomNum();
+    var maxNum = Math.max(num1, num2);
+    var minNum = Math.min(num1, num2);
+    var solution = maxNum - minNum;
+    numberOfRuns++;
+    var userNum = parseInt(prompt("Wieviel ist " + maxNum + " - " + minNum + " ?"));
+
+
+    if (userNum === solution) {
+        points += 1
+        alert("Richtige Antwort! " + points + "/5");
+        if (numberOfRuns < 5) {
+            subtraktion(points, numberOfRuns);
+        }
+        else if (numberOfRuns === 5 && points >= 4) {
+            return 0;
+        }
+        else if (numberOfRuns === 5 && points < 4) {
+            alert("Du hast leider nicht genug Punkte erreicht.")
+            addition();
+        }
+    }
+    else if (userNum !== solution) {
+        alert("Leider Falsch! " + points + "/5");
+        if (numberOfRuns < 5) {
+            subtraktion(points, numberOfRuns);
+        }
+        else if (numberOfRuns === 5 && points >= 4) {
+            return 0;
+        }
+        else if (numberOfRuns === 5 && points < 4) {
+            alert("Du hast leider nicht genug Punkte erreicht.")
+            addition();
+        }
+
+    }
+}
+
 addition();
+
